10c26bca982f62f1eef3eccedcbaaabbf0bf7e7e
Enchan <enchant0130@gmail.com>
2023-08-14 17:37:32 +0900

    [Delete] unused PreviewProvider class

--- ql-extension/PreviewViewController.swift
+++ ql-extension/PreviewViewController.swift
@@ -18,18 +18,6 @@ class PreviewViewController: NSViewController, QLPreviewingController {
         super.loadView()
         // Do any additional setup after loading the view.
     }
-
-    /*
-     * Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
-     *
-    func preparePreviewOfSearchableItem(identifier: String, queryString: String?, completionHandler handler: @escaping (Error?) -> Void) {
-        // Perform any setup necessary in order to prepare the view.
-        
-        // Call the completion handler so Quick Look knows that the preview is fully loaded.
-        // Quick Look will display a loading spinner while the completion handler is not called.
-        handler(nil)
-    }
-     */
     
     func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
         
