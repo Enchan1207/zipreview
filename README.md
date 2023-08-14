6913af1980220a4dad0f4dacfc6c0109183cf6d3
Enchan <enchant0130@gmail.com>
2023-08-14 16:54:45 +0900

    [Add] Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# zipreview
+
+## Ovewview
+
+Quicklook preview for archive files
+
+## License
+
+This repository is published under [MIT License](LICENSE).
---
+++ zipreview/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  zipreview
+//
+//  Created by EnchantCode on 2023/08/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
