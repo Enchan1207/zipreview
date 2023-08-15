e34ee672fe3cb192f0a809ffef0dec9125a31fb7
Enchan <enchant0130@gmail.com>
2023-08-15 11:50:24 +0900

    [Update] add package tarunon/Instantiate, configure basic ui

---
+++ ql-extension/View/OutlineCellView.swift
@@ -0,0 +1,20 @@
+//
+//  OutlineCellView.swift
+//  ql-extension
+//
+//  Created by EnchantCode on 2023/08/14.
+//
+
+import Cocoa
+import Instantiate
+import InstantiateStandard
+
+class OutlineCellView: NSTableCellView, NibInstantiatable {
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
