ded83bfc56b11589f2dd7174c3950744158e526e
Enchan <enchant0130@gmail.com>
2023-08-15 21:39:27 +0900

    [Add #3] outline cell view

--- ql-extension/View/OutlineCellView.swift
+++ ql-extension/View/OutlineCellView.swift
@@ -11,6 +11,15 @@ import InstantiateStandard
 
 class OutlineCellView: NSTableCellView, NibInstantiatable {
     
+    /// エントリの種類 (file/directory/symlink) を表すアイコン
+    @IBOutlet weak var entryKindIcon: NSImageView!
+    
+    /// エントリがリンクかどうかを表すインジケータ
+    @IBOutlet weak var linkIndicator: NSImageView!
+    
+    /// エントリ名
+    @IBOutlet weak var entryNameLabel: NSTextField!
+    
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
